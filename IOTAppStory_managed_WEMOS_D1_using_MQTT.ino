88fca18f4cbf6c1807e756c09feb3db1a2395afb
DocDoo <46087270+DocDoo@users.noreply.github.com>
2020-05-14 21:52:59 +0200

    Code clean up after learnings on my other app

--- IOTAppStory_managed_WEMOS_D1_using_MQTT.ino
+++ IOTAppStory_managed_WEMOS_D1_using_MQTT.ino
@@ -25,7 +25,8 @@
   virginSoilFull V2.2.2
 */
 
-String SketchVersion = "Doorbell activation sensor v1.1.8";
+char* SketchVersion = "Doorbell activation sensor v1.2.0";
+char* MQTT_Topic    = "HGV10/Huset/Fordoer/Ringklokke";
 
 #define COMPDATE __DATE__ __TIME__
 #define build_str  "Version: " __DATE__ " " __TIME__;
@@ -35,6 +36,7 @@ String SketchVersion = "Doorbell activation sensor v1.1.8";
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #define DOORBELL_BUTTON_PIN D5                              // Doorbell button pin (with 10k resistor connected to ground for pull-up)
+
 // Declare the variable as volatile to avoid the compiler optimizing it (and thus removing it from the programm)
 volatile bool DoorbellButtonPressedFlag = false;
 // Declare that the interupt should be chached in RAM
@@ -42,10 +44,9 @@ void ICACHE_RAM_ATTR DoorbellButtonPressed ();
 
 WiFiClient espClient;
 PubSubClient MQTTclient(espClient);
+#define MSG_BUFFER_SIZE (100)
+char msg[MSG_BUFFER_SIZE];
 
-// Declare functions needed for setup function to allow then to be referenced before they are declared
-bool SubscribeToMQTTTopic(char Topic[]);
-bool SendMQTTMessage(char Message[]);
 /* ------ NON IAS CODE SECTION END ------ */
 
 #include <IOTAppStory.h>                                    // IotAppStory.com library
@@ -62,8 +63,6 @@ bool DoorbellButtonPressedProcessingInProgress = false; // Determine if we are c
 // Use functions like atoi() and atof() to transform the char array to integers or floats
 // Use IAS.dPinConv() to convert Dpin numbers to integers (D6 > 14)
 
-char* WiFi_SSID        = "<WiFi SSID>";                 // INPUT NEEDED: I'm not sure how I need to handle WiFi in the non-IAS
-char* WiFi_Password    = "<WiFi Pwd>";                  // INPUT NEEDED: part of the sketch as IAS seems already to have WiFi initialized
 char* MQTT_Broker_IP   = "<MQTT Broker IP>";
 char* MQTT_Broker_Port = "0000";
 char* MQTT_Username    = "<MQTT Broker Username>";
@@ -118,127 +117,47 @@ void setup() {
   //-------- Your Setup starts from here ---------------
   Serial.begin(115200);  
 
-  //Setup WiFi connection
-  EnsureConnectionToWiFi();
-
   //Setup MQTT connection
   MQTTclient.setServer(MQTT_Broker_IP, atoi(MQTT_Broker_Port));
-  MQTTclient.setCallback(callback);
-
-  //Establish MQTTClient connection
-  EnsureConnectionToMQTTBroker();
+  MQTTclient.setCallback(subscribeReceive);
 
   //Setup board pins
   pinMode(DOORBELL_BUTTON_PIN, INPUT);
   attachInterrupt(DOORBELL_BUTTON_PIN, DoorbellButtonPressed, RISING);
   InteruptAttached = true;
-  
-  // Publish on MQTT topic, that this device has booted
-  Serial.println("Publishing announcement of device boot");
-  String bootMessage = "Wemos bootede: " + SketchVersion;
-  int bootMessage_len = bootMessage.length() + 1; 
-  char bootMessage_char_array[bootMessage_len];
-  bootMessage.toCharArray(bootMessage_char_array, bootMessage_len);
-  
-  while (!SendMQTTMessage(bootMessage_char_array)) {}  
-  Serial.println("Published OK");
 
-  Serial.println("Subscribing to topic HGV10/Huset/Fordoer/Ringklokke");
-  while (!SubscribeToMQTTTopic("HGV10/Huset/Fordoer/Ringklokke")) {}
-  Serial.println("Subscribed OK");
-}
-
-bool SendMQTTMessage(char Message[]) {
-  Serial.println("Ensuring WiFi is connected...");
-  if (EnsureConnectionToWiFi()) {
-    Serial.println("WiFi is connected, ensuring MQTT is connected...");
-    if (EnsureConnectionToMQTTBroker()) {
-      Serial.println("MQTT is connected, publishing message");
-      MQTTclient.publish("HGV10/Huset/Fordoer/Ringklokke", Message);
-      Serial.println("MQTT message was published!");
-    }
-  }  
-}
-
-bool SubscribeToMQTTTopic(char Topic[]) {
-  Serial.println("Ensuring WiFi is connected...");
-  if (EnsureConnectionToWiFi()) {
-    Serial.println("WiFi is connected, ensuring MQTT is connected...");
-    if (EnsureConnectionToMQTTBroker()) {
-      Serial.println("MQTT is connected, subscribing to topic");
-      MQTTclient.subscribe(Topic);
-      //Reset flag indicating is button was pressed
-    }
-  }    
-}
-
-bool EnsureConnectionToWiFi() {
-  Serial.println("Ensuring connection to WiFi...");  
-  if (WiFi.status() != WL_CONNECTED) {
-    WiFi.begin(WiFi_SSID, WiFi_Password);
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.println("WiFi connecting..");
-    }
-    Serial.println("WiFi connected OK");
-  }
-  return true;
-}
-
-bool EnsureConnectionToMQTTBroker() {
-    // If MQTT port has not yet been set, assume the user needs to configure the device and enter Config Mode
-  if ( atoi(MQTT_Broker_Port) == 0 ) {
-    Serial.println("MQTT Broker IP address is not yet set, entering Config Mode...");
-    IAS.espRestart('C');    
-  }  
-  if (millis() - MQTTConnectionFailedTimeout > 4000) {
-    Serial.println("MQTT connecting...");
-    Serial.print("MQTT Username = ");
-    Serial.println(MQTT_Username);
-    if (MQTTclient.connect("ESP8266Client", MQTT_Username, MQTT_Password )) {
-      Serial.println("MQTT connected OK");  
-      return true;
-    } else {
-      Serial.print("MQTT connection failed with state ");
-      Serial.print(MQTTclient.state());
-      MQTTConnectionFailedTimeout =  millis();
-      Serial.println("Enter Config mode to set MQTT Username");
-      return false;
-    }
-  }
+  Serial.println("Subscribing to MQTT topic");
+  MQTTclient.subscribe(MQTT_Topic);
+  Serial.println("Subscribed OK to MQTT topic");
+   
+  //Construct the message which will be published to the MQTT interface
+  snprintf(msg, MSG_BUFFER_SIZE, "WeMos D1 Mini - First loop of %s", SketchVersion);
+  Serial.println(msg);
+  publishToMQTT();  
 }
 
 void DoorbellButtonPressed() {
   int button = digitalRead(DOORBELL_BUTTON_PIN);
-  if(button == HIGH)
-  {
+  if(button == HIGH) {
     DoorbellButtonPressedFlag = true;
   }
   return;
 }
 
 void triggerDoorbellActiveEvent() {
-  SendMQTTMessage("Doerklokke aktiveret");
+  //Construct the message which will be published to the MQTT interface
+  snprintf(msg, MSG_BUFFER_SIZE, "Doerklokke aktiveret");
+  Serial.println(msg);
+  publishToMQTT();
 }
 
 void triggerDoorbellNotActiveEvent() {
-  SendMQTTMessage("Doerklokke deaktiveret");
+  //Construct the message which will be published to the MQTT interface
+  snprintf(msg, MSG_BUFFER_SIZE, "Doerklokke deaktiveret");
+  Serial.println(msg);
+  publishToMQTT(); 
 }
  
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived in topic: ");
-  Serial.println(topic);
- 
-  Serial.print("Message:");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
- 
-  Serial.println();
-  Serial.println("-----------------------");
-}
-
-
 // ================================================ LOOP =================================================
 void loop() {
   IAS.loop();                                   // this routine handles the calling home functionality and reaction of the MODEBUTTON pin. If short press (<4 sec): update of sketch, long press (>7 sec): Configuration
@@ -276,4 +195,71 @@ void loop() {
    
   // Check for incomming messages and retain the connection with the MQTT broker
   MQTTclient.loop();
+}
+
+// ================================================ Extra functions ======================================
+
+void subscribeReceive(char* topic, byte* payload, unsigned int length) {
+  char data[100]; // buffer for constructing Serial.Print messages
+  char* Received_MQTT_Topic = topic;
+  payload[length] = '\0';
+  char* Received_MQTT_Message = (char*)payload;
+
+  sprintf(data, "In topic [%s], this message arrived [%s]", Received_MQTT_Topic, Received_MQTT_Message);
+  Serial.println(data);
+
+  // Use this command to trigger the MQTT interface to put the WeMos D1 into IOTAppStory Configuration Mode
+  // Doing this will allow you to access its IOTAppStory configuration and reconfigure it before exiting configuration mode again
+  // http://192.168.1.11/admin/plugins/mqttgateway/mqtt.php?topic=HGV10/Huset/Fordoer/Ringklokke&value=IAS_Config_Start
+  
+  // Check if a command to enter Configuration Mode was received
+  if ((strcmp(Received_MQTT_Topic, MQTT_Topic) == 0) && (strcmp(Received_MQTT_Message, "IAS_Config_Start") == 0)) {
+    Serial.println("Received MQTT command to enter IAS Configuration Mode. Now restarting WeMos D1 Mini to do that.");
+    IAS.espRestart('C');        
+  }
+
+  Serial.println("-----------------------");
+}
+
+void reconnect() {
+  // If MQTT port has not yet been set, assume the user needs to configure the device and enter Config Mode
+  if ( atoi(MQTT_Broker_Port) == 0 ) {
+    Serial.println("MQTT Broker IP address is not yet set, entering Config Mode...");
+    IAS.espRestart('C');    
+  }  
+  
+  // Loop until we're reconnected
+  while (!MQTTclient.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (MQTTclient.connect(clientId.c_str(), MQTT_Username, MQTT_Password)) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      sprintf(msg, "WeMos D1 Mini - MQTT reconnected for %s", SketchVersion);
+      publishToMQTT();
+      // ... and resubscribe
+      MQTTclient.subscribe(MQTT_Topic);
+    } else {
+      Serial.print("MQTT reconnect failed, rc=");
+      Serial.print(MQTTclient.state());
+      Serial.println(" try again in 5 seconds");
+      Serial.println("If you want to enter Config mode, then press and connect D3 and GND between 4 and 7 secs starting in 2 seconds...");
+      // Wait 2 seconds for user to begin pressing button
+      delay(2000);
+      IAS.loop(); // this routine handles the calling home functionality and reaction of the MODEBUTTON pin. If short press (<4 sec): update of sketch, long press (>7 sec): Configuration 
+      delay(3000);
+    }
+  }
+}
+
+void publishToMQTT() {
+  if (!MQTTclient.connected()) {
+    reconnect();
+  }
+  MQTTclient.loop();
+  delay(2);
+  MQTTclient.publish(MQTT_Topic, msg);
 }
